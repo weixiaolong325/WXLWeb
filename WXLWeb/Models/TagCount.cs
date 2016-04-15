abb44c52eed440edcae5e68826cc48063acaf2e7
weixiaolong <284743503@qq.com>
2016-04-15 09:10:48 +0800

    update

---
+++ WXLWeb/Models/TagCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WXLWeb.Models
+{
+    public class TagCount
+    {
+        public string TagName { get; set; }
+        public string CountNum { get; set; }
+    }
+}
